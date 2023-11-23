4b57dd699741ed4e8cf263285822786c7b215853
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-11-24 04:50:53 +0600

    modify

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-var swiper = new Swiper(".slide-container", {
-    slidesPerView: 3,
+// Initialize Swiper
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
     spaceBetween: 30,
-    slidesPerGroup: 3,
     loop: true,
-    loopFillGroupWithBlank: true,
+    autoplay: {
+      delay: 2500, // Adjust the delay as needed (in milliseconds)
+      disableOnInteraction: false,
+    },
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      el: '.swiper-pagination',
+      clickable: true,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+  });
+  
